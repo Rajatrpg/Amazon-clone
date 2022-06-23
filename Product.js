f3905813964781f2e7a2080023156a6e9924dc29
Rajatrpg <96593277+Rajatrpg@users.noreply.github.com>
2022-06-23 16:20:48 +0530

    Add files via upload

---
+++ Product.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import './Product.css'
+import { useStateValue } from './StateProvider'
+
+export default function ProductCart({ id, title, price, rating, image }) {
+    const [{ basket }, dispatch] = useStateValue();
+    const removeItem = () => {
+        //Remove from basket...
+        dispatch({
+            type: "REMOVE_FROM_CART",
+            id: id,
+        })
+    };
+
+    return (
+        <div className="productcart">
+            <img className="productcart__image" src={image} alt="" />
+            <div className="productcart__info">
+                <p>{title}</p>
+                <p className="productcart__price">
+                    <small>$</small>
+                    <strong>{price}</strong>
+                </p>
+                <div className="product__rating">
+                    {Array(rating).fill().map((_) => (
+                        <p>*</p>
+                    ))}
+                </div>
+            </div>
+            <img src={image} alt="" />
+            <button onClick={removeItem}>Remove From basket</button>
+        </div>
+    )
+}
+
